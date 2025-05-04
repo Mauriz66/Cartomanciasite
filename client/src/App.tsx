030513de5e34932b34163165f0b7f9a92e2b345d
Mauriz66 <76722408+Mauriz66@users.noreply.github.com>
2025-05-04 06:25:02 -0300

    fix: remove temporariamente rotas e imports de Sobre e Contato para corrigir erro de build no Netlify

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,8 +13,8 @@ import LeituraAniversario from "@/pages/leitura-aniversario";
 import Thetahealing from "@/pages/thetahealing";
 import Reiki from "@/pages/reiki";
 import PlanoConexao from "@/pages/plano-conexao";
-import Sobre from "@/pages/Sobre";
-import Contato from "@/pages/Contato";
+// import Sobre from "@/pages/sobre";
+// import Contato from "@/pages/contato";
 import { Navbar } from "@/components/layout/Navbar";
 import { Footer } from "@/components/layout/Footer";
 import { WhatsAppButton } from "@/components/layout/WhatsAppButton";
@@ -40,8 +40,8 @@ function Router() {
         <Route path="/thetahealing" component={Thetahealing} />
         <Route path="/reiki" component={Reiki} />
         <Route path="/plano-conexao" component={PlanoConexao} />
-        <Route path="/sobre" component={Sobre} />
-        <Route path="/contato" component={Contato} />
+        {/* <Route path="/sobre" component={Sobre} /> */}
+        {/* <Route path="/contato" component={Contato} /> */}
         <Route component={NotFound} />
       </div>
       <Footer />
